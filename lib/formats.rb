6d046a2be2b770ffa67f6cc4ce171b874720c3f1
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2008-10-23 23:08:29 +0200

    adding fasta => nexus conversion tool
    This commit adds a fasta => nexus conversion tool, and adds for that read_fasta
    and write_tcs methods. Tests have also been added (and a small fix) for
    is_fasta?

--- lib/formats.rb
+++ lib/formats.rb
@@ -1,3 +1,29 @@
+def read_fasta(file)
+    if !is_fasta?(file)
+        raise ArgumentError, "#{file} is not a valid FASTA file"
+    end
+
+    result = Array.new
+    File.open(file) do |io|
+        line = io.readline.chomp
+        while !io.eof?
+            seqname = line.chomp[1..-1].strip
+
+            data = ""
+            line = io.readline.chomp
+            while !io.eof? && line !~ /^>/
+                data << line
+                line = io.readline.chomp
+            end
+            data << line if io.eof?
+
+            result << [seqname, data]
+        end
+    end
+    puts "found #{result.size} sequences"
+    result
+end
+
 def is_fasta?(file)
     line_size = nil
     File.open(file) do |io|
@@ -5,8 +31,13 @@ def is_fasta?(file)
 	
 	while !io.eof?
 	    line = io.readline.chomp
+            if line =~ /^>/
+                # New sequence, skip
+                next
+            end
+
 	    return false if line !~ /^[A-Za-z\-:]*$/
-	    
+
 	    if line_size && line.size != line_size
 		return (io.eof? || io.read =~ /^\n*$/)
 	    end
@@ -14,5 +45,50 @@ def is_fasta?(file)
 	end
 	return true
     end
+end
+
+def write_tcs(title, sequences, io)
+    if sequences.empty?
+        raise ArgumentError, "no sequences given"
+    end
+
+    seqlength = sequences.first.last.size
+    namewidth = sequences.map { |name, _| name.size }.max
+
+    sequences.each do |name, data|
+        if data.size != seqlength
+            raise ArgumentError, "sequence data mismatch: #{name} has #{data.size} bases instead of #{seqlength}"
+        end
+        if data =~ /[U]/
+            bad = (data =~ /[U]/)
+            raise ArgumentError, "#{name} contains U (character #{bad})"
+        end
+    end
+
+    io.puts <<-EOH
+#NEXUS\r
+[Title #{title}]\r
+begin taxa;\r
+dimensions ntax=#{sequences.size};\r
+taxlabels\r
+\r
+#{sequences.map { |name, _| "%- #{namewidth}s" % name }.join("\r\n")}\r
+\r
+;\r
+end;\r
+begin characters;\r
+dimensions nchar=#{seqlength};\r
+FORMAT DATATYPE=DNA MISSING=? GAP=-;\r
+matrix\r
+\r
+[!Domain=Data;]\r
+\r
+    EOH
+
+    sequences.each do |name, data|
+        io.puts "%- #{namewidth}s %s\r" % [name, data.upcase]
+    end
+
+    io.print ";\r\nend;"
 end
 
--- test/test_formats.rb
+++ test/test_formats.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../lib', File.dirname(__FILE__))
 require 'test/unit'
 require 'formats'
+require 'stringio'
 
 class TC_Formats < Test::Unit::TestCase
     BASEDIR      = File.expand_path(File.dirname(__FILE__))
@@ -12,4 +13,60 @@ class TC_Formats < Test::Unit::TestCase
 	    end
 	end
     end
+
+    OSMU6173_SEQ = "TTAATGCTTTCGCTAAGTCACGCACGGTGTATTGTGCACAACGAGTAATCATCGTTTACGGCGTGGACTACCAGGGTATCTAATCCTGTTCGCTCCCCACGCTTTCGTCCATCAGCGTCAATAATGGTTTAGTAAGCTGCCTTCGCAATTGGTGTTCTACGTTATATCTATGCATTTCACCGCTACATAACGTATTCCGCCTACCTCAACCATATTCAAGTCTTTCAGTTTCAATGGCAGTTCCAGAGTTGAGCTCTGGGATTTCACCACTGACTTAAAAGACCGCCTACGGACCCTTTAAACCCAATAAATCCGGATA:A:CGCTCGAATCCTCCGTATTACCGCGGCTGCTGGCACGGAGTTAGCCGATCCTTATTCATACAGTACATTCAAACTTCTACACGTAGAAGCAATTATTCCTGT"
+
+    TEST4TCS_SEQNAMES = ["Osmu3-93A",
+                "Osmu3-71A",
+                "Osmu3-69A",
+                "Osmu3-61A",
+                "Osmu3-51A",
+                "Osmu3-50A",
+                "Osmu3-37A",
+                "OSmu3-30A",
+                "Osmu3-70A",
+                "Osmu1-274A",
+                "Osmu1-324A",
+                "Osmu1-251A",
+                "OT19-3398A",
+                "OT19-3401A",
+                "OT1-1002D",
+                "OT1-1046A",
+                "OT1-1050D",
+                "OT1-1075",
+                "OT2-1427A",
+                "OT3-3114A",
+                "OT3-3124A",
+                "OT3-3152A",
+                "OT3-3156A",
+                "OT4-1781A",
+                "OT4-1782A",
+                "OT73-3868F",
+                "OT75-40832",
+                "OT76-2687E"]
+
+    def test_read_fasta
+        data = read_fasta File.join(BASEDIR, "fasta_set", "Osmu6_173.TXT")
+        assert_equal 1, data.size
+        assert_equal "Osmu6173", data.first.first
+        assert_equal OSMU6173_SEQ, data.first.last
+
+        data = read_fasta File.join(BASEDIR, "fasta2tcs", "test4tcs.fas")
+        assert_equal 28, data.size
+        assert_equal TEST4TCS_SEQNAMES, data.map { |name, _| name }
+        assert data.all? { |_, data| data.size == 1419 }
+    end
+
+    def test_write_tcs
+        data = read_fasta File.join(BASEDIR, "fasta2tcs", "test4tcs.fas")
+
+        output = StringIO.new
+        write_tcs "osedaxmucofloris_oceanosymbiontgroupA", data, output
+        File.open 'test.out', 'w' do |io|
+            io.write output.string
+        end
+
+        assert_equal File.read(File.join(BASEDIR, "fasta2tcs", "test4tcs.txt")), output.string
+    end
 end
+
